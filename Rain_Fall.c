3ebf266f2e4b75ebe5dffd2facf4dd752577752e
amithsivakoti74 <147487933+amithsivakoti74@users.noreply.github.com>
2023-12-30 22:40:36 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x<3)     {         printf("LIGHT");         }         else if(x>=7)         {             printf("HEAVY");             }             else if(2<x<7)             {                 printf("MODERATE");    }
    }

--- Rain_Fall.c
+++ Rain_Fall.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int x;
+    int x;  
     scanf("%d",&x);
     if(x<3)
     {
         printf("LIGHT");
         }
-        else if(x>=7)  
+        else if(x>=7) 
         {
             printf("HEAVY");
             }
-            else if(2<x<7)
+            else if(2<x<7) 
             {
                 printf("MODERATE");    }
     
