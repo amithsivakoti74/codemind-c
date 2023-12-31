43b40f681594041db4a1d155434c7a4826f74267
amithsivakoti74 <147487933+amithsivakoti74@users.noreply.github.com>
2023-12-31 23:16:02 +0530

    #include<stdio.h> int main() {     int n,i,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(i*3)         {
            }
                printf("%d ",i*3);
        }
    }

---
+++ First_N_multiples_of_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i*3)
+        {
+        
+        }
+            printf("%d ",i*3);
+    }
+}
